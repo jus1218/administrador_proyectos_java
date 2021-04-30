481ce14ddb1a8e7beca2b3423c4b2e6386336220
jus1218 <38591716+jus1218@users.noreply.github.com>
2021-04-29 20:18:43 -0600

    Update Main.java

--- src/main/java/com/mycompany/administradordeproyectos/Main.java
+++ src/main/java/com/mycompany/administradordeproyectos/Main.java
@@ -1,16 +1,1 @@
-package com.mycompany.administradordeproyectos;
 
-/**
- *
- * @author JUS
- */
-public class Main {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        System.out.println("Hola people");
-    }
-    
-}
